e6917ad16f1a71b575c3671183831bfca8a605f5
glener10 <glenerpizzolato@gmail.com>
2025-09-16 12:55:28 -0300

    feat: add env.py for loading environment variables and API key management

---
+++ src/env.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+def get_api_key():
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+    if not api_key:
+        raise Exception("❌ missing API_KEY in .env")
+    return api_key
